c8120ab8bb57f94b21c3cace8cb297cb5853f22e
SinLess-Games <56801143+SinLess-Games@users.noreply.github.com>
2023-05-26 04:56:59 -0600

    :rocket: Intial

---
+++ src/main/java/com/mobiusinfernium/MobiusInfernium.java
@@ -0,0 +1,17 @@
+package com.mobiusinfernium;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobiusInfernium extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
